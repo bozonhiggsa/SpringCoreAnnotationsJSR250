8640914a28a79996092fe0ff35fd08b105c9cd87
bozonhiggsa <myrealjobandmoney>
2018-02-16 18:45:46 +0200

    minimal editing

--- src/main/java/com/springApp/springIoC/MessageRunner.java
+++ src/main/java/com/springApp/springIoC/MessageRunner.java
@@ -3,6 +3,12 @@ package com.springApp.springIoC;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ Entry point
+ @author Ihor Savchenko
+ @version 1.0
+ */
+
 public class MessageRunner {
     public static void main(String[] args) {
         AbstractApplicationContext context =
