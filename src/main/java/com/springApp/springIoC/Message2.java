c2030b48e0a66ce2aeb4010a17efea5b479268d2
Ihor Savchenko <ihor.v.savchenko@gmail.com>
2019-04-03 18:18:59 +0300

    minimal editing

--- src/main/java/com/springApp/springIoC/Message2.java
+++ src/main/java/com/springApp/springIoC/Message2.java
@@ -1,19 +1,18 @@
 package com.springApp.springIoC;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-public class Message {
+public class Message2 {
     private String message;
 
-    public Message() {
-        System.out.println("Constructor of Message is performed");
+    public Message2() {
+        System.out.println("Constructor of Message2 is performed");
     }
 
     public String getMessage() {
-        return "Message: " + message;
+        return "Message2: " + message;
     }
 
     public void setMessage(String message) {
@@ -23,12 +22,12 @@ public class Message {
     //analog init-method="beanInit" in xml-configuration
     @PostConstruct
     public void beanInit() {
-        System.out.println("Bean of Message is passing init stage.");
+        System.out.println("Bean of Message2 is passing init stage.");
     }
 
     //analog destroy-method="beanDestroy" in xml-configuration
     @PreDestroy
     public void beanDestroy() {
-        System.out.println("Bean of Message is passing destroy stage.");
+        System.out.println("Bean of Message2 is passing destroy stage.");
     }
 }
--- src/main/java/com/springApp/springIoC/MessageRunner.java
+++ src/main/java/com/springApp/springIoC/MessageRunner.java
@@ -14,9 +14,25 @@ public class MessageRunner {
         AbstractApplicationContext context =
                 new ClassPathXmlApplicationContext("annotations-jsr250-config.xml");
 
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Message message = (Message) context.getBean("message");
         System.out.println(message.getMessage());
 
-        context.registerShutdownHook();
+        context.close();
+
+        context =
+                new ClassPathXmlApplicationContext("annotations-jsr250-config.xml");
+
+        message = (Message) context.getBean("message");
+        System.out.println(message.getMessage());
+        Message2 message2 = (Message2) context.getBean("message2");
+        System.out.println(message2.getMessage());
+
+        context.close();
     }
 }
